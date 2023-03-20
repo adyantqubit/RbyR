5c28ee3caafee5405925a942ca405cbecea39a36
info@adyant.co.in <info@adyant.co.in>
2023-03-19 23:23:25 -0700

    push by rohan

--- site_setup/site_setup/settings.py
+++ site_setup/site_setup/settings.py
@@ -163,8 +163,10 @@ Base_url="http://192.168.1.50:3000"
 
 
 import os
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
-
+STATIC_URL = '/static/'
+STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- site_setup/site_setup/urls.py
+++ site_setup/site_setup/urls.py
@@ -3,8 +3,10 @@ from django.contrib import admin
 from django.urls import path,include
 from back_site import views
 import back_site.views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('back_site.urls')),
-]
+    path('api/',include('back_site.urls')),
+]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)   
